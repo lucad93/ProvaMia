bc355ff5921eea72d835bbd8976eac3bd138a617
Stefano <Stefano@192.168.1.84>
2016-03-28 20:37:52 +0200

    Aggiunto commento

--- prova/src/prova/Prova.java
+++ prova/src/prova/Prova.java
@@ -17,6 +17,7 @@ public class Prova {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Ciao");
+        //Fine del file
     }
     
 }
